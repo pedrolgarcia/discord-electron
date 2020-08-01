e2efe744cdbcb68d187805542acbd01ceee6dad6
Pedro Garcia <pedrolopes.info@gmai.com>
2020-08-01 18:46:54 -0300

    feat: channel messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
 
+import ChannelMessage, { Mention } from '../ChannelMessage'
+
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>
+
+  useEffect(() => {
+    const div = messagesRef.current
+
+    if (div) div.scrollTop = div.scrollHeight
+  }, [messagesRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        <ChannelMessage
+          author="Pedro Lopes"
+          date="01/08/2020"
+          content="Hello World"
+        />
+        <ChannelMessage
+          author="Chronon"
+          date="01/08/2020"
+          content={
+            <>
+              <Mention>@PedroLoG</Mention>, me carrega no
+                            Overwatch ai na moral!
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre" />
         <InputIcon />
